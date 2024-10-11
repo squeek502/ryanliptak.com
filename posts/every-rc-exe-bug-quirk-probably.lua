24953d5cd4872b31552716f2cd0723f4c30b2571
Ryan Liptak <squeek502@hotmail.com>
2024-10-11 16:06:18 -0700

    Fix publish date

--- posts/every-rc-exe-bug-quirk-probably.lua
+++ posts/every-rc-exe-bug-quirk-probably.lua
@@ -1,6 +1,6 @@
 return {
   title = "Every bug/quirk of the Windows resource compiler (rc.exe), probably",
-  date = "2024-10-12",
+  date = "2024-10-11",
   description = "Fuzz testing decades-old software can turn up some curious behaviors",
   keywords = "zig, win32, fuzzing",
   categories = {"Programming", "Fuzzing"},
