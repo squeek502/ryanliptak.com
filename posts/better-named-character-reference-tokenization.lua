bcb196b7c302cb098e8f022c5b2f9ebe01a8390d
Ryan Liptak <squeek502@hotmail.com>
2025-04-09 03:42:07 -0700

    wip named character reference post

---
+++ posts/better-named-character-reference-tokenization.lua
@@ -0,0 +1,8 @@
+return {
+  title = "Slightly better named character reference tokenization than Chrome, Safari, and Firefox",
+  subtitle = "Just as (or slightly more) efficient, and more compact and easy to use",
+  date = "2025-03-22",
+  description = "Stumbling into a (slightly) better HTML named reference tokenization implementation",
+  keywords = "html, parsing, tokenization, blink, gecko, webkit, firefox, chrome, safari, ladybird, libweb",
+  categories = {"Programming"},
+}
