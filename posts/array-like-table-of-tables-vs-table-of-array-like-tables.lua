3148a57f3d430e1b157c9b85699302a206935194
Ryan Liptak <squeek502@hotmail.com>
2025-04-09 03:37:36 -0700

    Add skeleton for something I might write, dunno

---
+++ posts/array-like-table-of-tables-vs-table-of-array-like-tables.lua
@@ -0,0 +1,7 @@
+return {
+  title = "Array-like Table of Tables vs Table of Array-like Tables",
+  date = "2025-03-22",
+  description = "DOD in Lua",
+  keywords = "lua",
+  categories = {"Programming", "Lua"},
+}
